396530fb83b4b10103701284d130862ab385df6d
AJAY <ajay@gmail.com>
2022-07-02 00:13:28 +0530

    added plugin

---
+++ Category-Service/src/test/java/com/ecart/delivery/service/CategoryServiceImplTest.java
@@ -0,0 +1,46 @@
+package com.ecart.delivery.service;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import com.ecart.delivery.dao.CategoryDao;
+import com.ecart.delivery.dto.CategoryDTO;
+import com.ecart.delivery.model.Category;
+
+@ExtendWith(MockitoExtension.class)
+class CategoryServiceImplTest {
+
+	@Mock
+	CategoryDao categoryDao;
+
+	@InjectMocks
+	CategoryServiceImpl categoryServiceImpl;
+
+	private CategoryDTO categoryDTO;
+
+	private Category category;
+
+	@BeforeEach
+	void beforeEach() {
+		categoryDTO = new CategoryDTO();
+		categoryDTO.setId(101);
+		categoryDTO.setCategoryName("WATCH");
+
+		category = new Category();
+		category.setId(categoryDTO.getId());
+		category.setCategoryName(categoryDTO.getCategoryName());
+	}
+
+	@Test
+	void addNewCategoryDetails() {
+		when(categoryDao.save(category)).thenReturn(category);
+		assertEquals("New Category Added Succesfully", categoryServiceImpl.addNewCategory(categoryDTO));
+	}
+}
